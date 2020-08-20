6f7e0577b374b7ea43eae8407dff5d9b613cfdc9
Raffael <mail@raffael-vogler.de>
2020-08-20 15:32:32 +0200

    test for api::get_story() refactored

--- tests/test_api_get_story.py
+++ tests/test_api_get_story.py
@@ -1,29 +1,32 @@
 import pickle
+from pathlib import Path
 
 import requests_mock
 
 from hackernews_api import api
 
 
-def test_external_story():
-    id = 23885927
-    f_hn = open("./tests/assets/api_get_story_external_0/hn_source_code.html")
-    f_api = open("./tests/assets/api_get_story_external_0/api_response.json")
-    f_ex = open("./tests/assets/api_get_story_external_0/expected_return_value.pickle", "rb")
+def _test_story_helper(id: int, base_path_assets: Path):
+    with open(base_path_assets / "hn_source_code.html") as f_hn:
+        hn_html = f_hn.read()
+    with open(base_path_assets / "api_response.json") as f_api:
+        api_json = f_api.read()
+    with open(base_path_assets / "expected_return_value.pickle", "rb") as f_ex:
+        ex_obj = pickle.load(f_ex)
 
     with requests_mock.Mocker() as m:
-        m.get(f"https://news.ycombinator.com/item?id={id}", text=f_hn.read())
-        m.get(f"https://hacker-news.firebaseio.com/v0/item/{id}.json", text=f_api.read())
-        assert pickle.load(f_ex) == api.get_story(story_id=id)
+        m.get(f"https://news.ycombinator.com/item?id={id}", text=hn_html)
+        m.get(f"https://hacker-news.firebaseio.com/v0/item/{id}.json", text=api_json)
+        assert ex_obj == api.get_story(story_id=id)
+
+
+def test_external_story():
+    _test_story_helper(
+        id=23885927, base_path_assets=Path("./tests/assets/api_get_story_external_0"),
+    )
 
 
 def test_internal_story():
-    id = 23891838
-    f_hn = open("./tests/assets/api_get_story_internal_0/hn_source_code.html")
-    f_api = open("./tests/assets/api_get_story_internal_0/api_response.json")
-    f_ex = open("./tests/assets/api_get_story_internal_0/expected_return_value.pickle", "rb")
-
-    with requests_mock.Mocker() as m:
-        m.get(f"https://news.ycombinator.com/item?id={id}", text=f_hn.read())
-        m.get(f"https://hacker-news.firebaseio.com/v0/item/{id}.json", text=f_api.read())
-        assert pickle.load(f_ex) == api.get_story(story_id=id)
+    _test_story_helper(
+        id=23891838, base_path_assets=Path("./tests/assets/api_get_story_internal_0"),
+    )
